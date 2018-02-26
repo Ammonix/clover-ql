55bf69e970a74a5514d1a752420a999b5513962f
Ammonix <joan.kuenzler@gmail.com>
2018-02-26 22:19:12 +0100

    bugfix

--- src/services/thread.service.ts
+++ src/services/thread.service.ts
@@ -4,17 +4,24 @@ class ThreadService {
   public getThreads = (board: string): Promise<any[]> =>
     fourChanClient
       .getJSONFromRelativeURL<any>(`/${board}/threads.json`)
-      .then(this.flatten);
+      .then(this.flatten)
+      .then(threads => this.addAdditionalInformation(threads, board));
 
   public getThread = (board: string, thread: number): Promise<any> =>
-    this.getThreads(board).then(threads => ({
-      ...threads.find(post => post.no === thread),
-      boardId: board
-    }));
-  private flatten(posts: any[]): any[] {
+    this.getThreads(board).then(threads =>
+      threads.find(post => post.no === thread)
+    );
+  private flatten(pages: any[]): any[] {
     let arr: any[] = [];
-    for (const post of posts) {
-      arr = arr.concat(post.threads);
+    for (const page of pages) {
+      arr = arr.concat(page.threads);
+    }
+    return arr;
+  }
+  private addAdditionalInformation(threads: any[], board: string): any {
+    let arr: any[] = [];
+    for (const thread of threads) {
+      arr = arr.concat({ ...thread, boardId: board });
     }
     return arr;
   }
